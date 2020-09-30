3dc21d842d6e0849a26306e67c5065167288f36c
Fajeeer <49624833+Fajeeer@users.noreply.github.com>
2020-09-30 17:55:36 +1300

    Created login page that is React compatible (#5)
    This commit creates the login page with the respective login
    fields. This has been converted from regular HTML and CSS to be
    compatible with React. A new favicon has been implemented as
    well.

--- frontend-react/src/components/landingpage.js
+++ frontend-react/src/components/landingpage.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import Logo from '../resources/logo.png';
 import '../style/landingpage.css';
 
 class LandingPage extends Component {
-    render() {
-        return (
-            <div><h1 className="code">Landing Page</h1></div>
+    handleClick = () => {this.props.history.push("/dashboard");}
+    render(){
+        return(
+            <div id="content">
+                <div id="image">
+                    <img id="logo" src={Logo} alt="Logo"></img>
+                </div>
+                <div id="rightContent">
+                    <h1 id="mainTitle">UFDL <br /> Dataset Annotator</h1>
+                    <div id="loginForm">
+                        <form id="registerform">
+                            <input className="formtext" id="formuser" type="text" name="user" placeholder="Username"></input>
+                            <input className="formtext" id="formpass" type="password" name="password" placeholder="Password"></input>
+                            <input className="formtext" id="formtext" type="text" name="port" placeholder="Port"></input>
+                            <input className="formtext" id="formip" type="text" name="ipaddress" placeholder="IP Address"></input>
+                        </form>
+                        <button id="button" onClick={this.handleClick}> Login </button>
+                    </div>
+                    <hr />
+                    <p id="projectTitle">User Friendly Deep Learning</p>
+                </div>
+            </div>
         );
     }
 }
-
 export default LandingPage;
